4de6ef1e8609398a688d2a7ef2af9ff66dd5ef09
Brandon Leeds <brandonleeds@gmail.com>
2018-02-15 20:13:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    erb :error if !User.exists?(username: params[:username])
     @user = User.find_by(username: params[:username])
     session[:user_id] = @user.id
-    binding.pry
-    if @user == nil
-      erb :error
-    else
-      redirect '/account'
-    end
+    redirect '/account'
   end
 
   get '/account' do
